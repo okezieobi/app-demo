c1524b8c80416afa2a3960ddf38990143c0dac0d
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2023-04-08 20:47:59 +0100

    prettier fix 5

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { json, urlencoded } from 'express';
-import { join } from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index';
+import indexRouter from "./routes/index";
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(json());
 app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+app.use("/", indexRouter);
 
-  export default app;
+export default app;
