f64bc8e21f0b07dce0483ecc66a6b08a7a24b901
Frank Okezie Obiedere <obiedereokezie@gmail.com>
2023-04-15 02:07:46 +0100

    saving progress

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # app-demo
+https://backdrop-photo.notion.site/Backdrop-Backend-Engineer-Application-783e7e5bc8594a79980485cbf68d6e04
---
+++ src/models/Users.ts
@@ -0,0 +1,46 @@
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  Default,
+  HasOne,
+} from "sequelize-typescript";
+
+export interface UserAttributes {
+  id: string;
+  account_number?: number;
+  bank_code?: number;
+  account_name?: string;
+  is_verIfied?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+@Table({ tableName: "users", timestamps: true })
+export class Users
+  extends Model<UserAttributes, UserAttributes>
+  implements UserAttributes
+{
+  @Default(DataType.UUIDV4)
+  @Column({ primaryKey: true, type: DataType.UUID })
+  id!: string;
+
+  @Column({ allowNull: true, type: DataType.DATE })
+  createdAt?: Date;
+
+  @Column({ allowNull: true, type: DataType.DATE })
+  updatedAt?: Date;
+
+  @Column({ allowNull: true, type: DataType.INTEGER })
+  account_number?: number;
+
+  @Column({ allowNull: true, type: DataType.INTEGER })
+  bank_code?: number;
+
+  @Column({ allowNull: true, type: DataType.STRING })
+  account_name?: string;
+
+  @Column({ allowNull: true, type: DataType.BOOLEAN })
+  is_verIfied?: boolean;
+}
